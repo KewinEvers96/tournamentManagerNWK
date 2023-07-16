8b2b29f5e4dabe9e6bed568e2fc03266ae1d9371
KewinEvers11 <kewin4@gmail.com>
2023-07-16 01:26:10 -0500

    cambios y requirements uploaded

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import TournamentListView, TournamentCreateView, TournamentDetailView, TournamentInscriptionView, TournamentUninscriptionView
-from .views import MatchListView, MatchDetailView
+from .views import MatchListView, MatchDetailView, GenerateMatchList
 
 app_name = 'tournament'
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('tournaments/<int:pk>', TournamentDetailView.as_view(), name='tournament_detail'),
     path('tournaments/<int:pk>/enroll', TournamentInscriptionView.as_view(), name='tournament_inscription'),
     path('tournaments/<int:pk>/unroll', TournamentUninscriptionView.as_view(), name= 'tournament_uninscription'),
+    path('tournaments/<int:pk>/generate_matches', GenerateMatchList.as_view(), name= 'generate_matchlist'),
     path('matches', MatchListView.as_view(), name="all_match_list"),
     path('matches/<int:pk>', MatchDetailView.as_view(), name="match_detail")
 ]
